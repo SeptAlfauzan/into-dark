17fd7c9355a3b9d834c146a11279e7164c9b9ce2
SeptAlfauzan <alfauzansepta@gmail.com>
2020-12-19 01:31:03 +0700

    initial commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called asdbefore the first frame update
+    Vector2 movementInput;
+    public float speed;
+    Rigidbody2D playerRb;
+    void Start()
+    {
+        Debug.Log("Starting");
+        playerRb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput.x = Input.GetAxis("Horizontal");
+        movementInput.y = Input.GetAxis("Vertical");
+        
+    }
+
+    private void FixedUpdate() {
+        Vector2 movement = new Vector2(movementInput.x * speed, movementInput.y * speed);
+        movement *= Time.fixedDeltaTime;
+
+        playerRb.MovePosition(playerRb.position + movement);
+    }
+}
---
+++ Assets/WeaponBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D weaponRb;
+    public Camera camera;
+    Vector2 mousePos;
+    SpriteRenderer playerSpriteRenderer;
+    SpriteRenderer spriteRenderer;
+    void Start()
+    {
+        weaponRb = GetComponent<Rigidbody2D>();
+        camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+
+        playerSpriteRenderer = this.transform.parent.GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate() {
+        Vector2 lookDir = mousePos - weaponRb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 0f;
+        Debug.Log(angle);
+        weaponRb.rotation = angle;
+
+        ChangeDirectionPlayerAndWeapon(angle);    
+    }
+
+    void ChangeDirectionPlayerAndWeapon(float angle){
+        if(angle < -90f || angle > 90f){
+            playerSpriteRenderer.flipX = true;
+            spriteRenderer.flipY = true;
+        }else{
+            playerSpriteRenderer.flipX = false;
+            spriteRenderer.flipY = false;
+        }
+    }
+}
