c8b2f08e0d7784a4bb8bee9fa9b0697b011098ed
SeptAlfauzan <alfauzansepta@gmail.com>
2020-12-19 10:55:01 +0700

    add equip weapon mechanic

---
+++ Assets/ItemsBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemsBehavior : MonoBehaviour
+{
+    public bool IsEquiped;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // IgnoreCollisionPlayer();//ignore collision from item object with player
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // void IgnoreCollisionPlayer(){
+    //     Collider2D[] playerCollisions = GameObject.FindGameObjectWithTag("Player").GetComponents<Collider2D>();
+
+    //     foreach (Collider2D collision in playerCollisions)
+    //     {
+    //         Physics2D.IgnoreCollision(collision, GetComponent<Collider2D>());
+    //     }
+    // }
+}
--- Assets/WeaponBehavior.cs
+++ Assets/WeaponBehavior.cs
@@ -10,36 +10,51 @@ public class WeaponBehavior : MonoBehaviour
     Vector2 mousePos;
     SpriteRenderer playerSpriteRenderer;
     SpriteRenderer spriteRenderer;
+    Transform parent;
+
+    bool isEquiped = false;
     void Start()
     {
         weaponRb = GetComponent<Rigidbody2D>();
         camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
 
-        playerSpriteRenderer = this.transform.parent.GetComponent<SpriteRenderer>();
+        
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //check whether this weapon object has parent object(equiped by player) or not
+        if (transform.parent != null && transform.parent.tag == "Player")
+        {
+            isEquiped = true;
+            playerSpriteRenderer = this.transform.parent.GetComponent<SpriteRenderer>();
+        }else{
+            isEquiped = false;
+        }
+        GetComponent<ItemsBehavior>().IsEquiped = isEquiped;//update is equiped status in Items Behavior scripts
+        
         mousePos = camera.ScreenToWorldPoint(Input.mousePosition);
     }
 
     private void FixedUpdate() {
         Vector2 lookDir = mousePos - weaponRb.position;
         float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 0f;
-        Debug.Log(angle);
-        weaponRb.rotation = angle;
 
-        ChangeDirectionPlayerAndWeapon(angle);    
+        if (isEquiped)
+        {//if this weapon is equiped change rotation and direction of it base on mouseposition    
+            weaponRb.rotation = angle;
+            ChangeDirectionPlayerAndWeapon(angle, playerSpriteRenderer);    
+        }
     }
 
-    void ChangeDirectionPlayerAndWeapon(float angle){
+    void ChangeDirectionPlayerAndWeapon(float angle, SpriteRenderer playerSprite){
         if(angle < -90f || angle > 90f){
-            playerSpriteRenderer.flipX = true;
+            playerSprite.flipX = true;
             spriteRenderer.flipY = true;
         }else{
-            playerSpriteRenderer.flipX = false;
+            playerSprite.flipX = false;
             spriteRenderer.flipY = false;
         }
     }
