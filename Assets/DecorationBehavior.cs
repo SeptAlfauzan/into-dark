973dbacf8ee7f77a1ec880b4df60e36d61a5aaad
SeptAlfauzan <alfauzansepta@gmail.com>
2020-12-20 13:16:28 +0700

    add decoration behavior

---
+++ Assets/DecorationBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecorationBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    float transparancy = 100f;
+    Color transparent;
+    Color normal;
+    void Start()
+    {
+        transparent = GetComponent<SpriteRenderer>().color;
+        normal = GetComponent<SpriteRenderer>().color;
+        transparent.a = 0.5f;
+        normal.a = 1f;
+    }
+
+    private void OnTriggerStay2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().color = transparent;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collider) {
+        if (collider.gameObject.tag == "Player")
+        {
+            GetComponent<SpriteRenderer>().color = normal;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
